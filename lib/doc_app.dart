7cf002886fc76acb9810e92d994d1deb00db873e
MaznenFathy <116312190+MaznenFathy@users.noreply.github.com>
2024-01-13 15:06:50 +0200

    splash screen

--- lib/doc_app.dart
+++ lib/doc_app.dart
@@ -14,7 +14,7 @@ class DocApp extends StatelessWidget {
       designSize: const Size(375, 812),
       minTextAdapt: true,
       child: MaterialApp(
-        title: 'Docdoc App',
+        title: 'Docdoc sApp',
         theme: ThemeData(
           primaryColor: ColorManager.mainBlue,
           scaffoldBackgroundColor: Colors.white,
